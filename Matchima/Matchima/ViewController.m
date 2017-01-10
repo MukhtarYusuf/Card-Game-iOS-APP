48c6df61d2c039806c44266ce9c5ede94bfb0977
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2017-01-10 03:19:06 -0500

    Base Functionalities

---
+++ Matchima/Matchima/ViewController.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  Matchima
+//
+//  Created by Mukhtar Yusuf on 1/9/17.
+//  Copyright © 2017 Mukhtar Yusuf. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "PlayingCardView.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
+@property (strong, nonatomic) Deck *deck;
+
+@end
+
+@implementation ViewController
+
+- (Deck *)deck{
+    if(!_deck)
+        _deck = [[PlayingCardDeck alloc] init];
+    return _deck;
+}
+
+- (void) drawRandomCard{
+    Card *card = [self.deck drawRandomCard];
+    if([card isKindOfClass:[PlayingCard class]]){
+        PlayingCard *playingCard = (PlayingCard *)card;
+        self.playingCardView.rank = playingCard.rank;
+        self.playingCardView.suit = playingCard.suit;
+    }
+}
+
+- (void)touch:(UITapGestureRecognizer *)sender{
+    if(!self.playingCardView.faceUP)
+        [self drawRandomCard];
+    self.playingCardView.faceUP = !self.playingCardView.faceUP;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.playingCardView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(touch:)]];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
