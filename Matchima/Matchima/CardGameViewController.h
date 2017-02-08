6385ecaf565ac7972fbb05ed59db09c21df0e5b2
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2017-02-08 02:34:51 -0500

    Changes to base view controller

--- Matchima/Matchima/CardGameViewController.h
+++ Matchima/Matchima/CardGameViewController.h
@@ -7,13 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CardMatchingGame.h"
+#import "Deck.h"
+#import "PlayingCard.h"
+#import "SetCard.h"
 #import "Grid.h"
 #import "MyGrid.h"
 #import "playingCardView.h"
 #import "SetCardView.h"
 
 @interface CardGameViewController : UIViewController
+@property (strong, nonatomic) IBOutletCollection(UIView) NSMutableArray *cardViews;
 @property (weak, nonatomic) IBOutlet UIView *cardContainerView;
 @property (strong, nonatomic) Grid *gridForCards; //Protected for subclasses
 @property (strong, nonatomic) MyGrid *myGridForCards; //Protected for subclasses
+@property (strong, nonatomic) NSMutableArray *cardViewFrames; //Protected for subclasses
 @end
